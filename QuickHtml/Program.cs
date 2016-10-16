b73fdf7076363d3319a8e918b11aa1ec50d5883c
michelc <michelc@gmail.com>
2016-10-16 10:07:36 +0200

    Replace "oe" french character.

--- QuickHtml/Program.cs
+++ QuickHtml/Program.cs
@@ -396,11 +396,13 @@ namespace QuickHtml
         public static string FrenchChars(string text)
         {
             // Replace some punctuations with better equivalent
+            // Replace some characters with better equivalent
             if (text == null) return text;
 
             text = text.Replace("'", "’");
             text = text.Replace("...", "…");
             text = Regex.Replace(text, @"([\s,])(--)([\s,])", "$1–$3", RegexOptions.Multiline);
+            text = text.Replace("oe", "œ");
 
             return text;
         }
